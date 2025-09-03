92f86e1e1f851806eaa7a5f4fab0c449b44f06b3
Me <ericrvaa@gmail.com>
2025-09-02 21:13:38 -0400

    vite config

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: './', // Use relative paths for assets
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
+})
